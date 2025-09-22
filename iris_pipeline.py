bb3ad3f81aa59d37bedcb996f6fd9bfb83f2d32a
ekbarkacha <emmanuel.barkacha@aims.ac.rw>
2025-09-22 15:42:57 +0000

    part 3, task 1

---
+++ iris_pipeline.py
@@ -0,0 +1,9 @@
+from sklearn import datasets
+import pandas as pd
+
+iris = datasets.load_iris()
+
+df = pd.DataFrame(data=iris.data, columns=iris.feature_names)
+
+print(df.info())
+print(df.head())
